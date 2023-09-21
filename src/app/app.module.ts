131e8449a237833faec8c1376cf7afbc3830fca3
Thomas Baloyi <baloyi.thomas@outlook.com>
2023-09-21 16:36:13 +0200

    Generate components for rxjs and signals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RxjsComponent } from './components/rxjs/rxjs.component';
+import { SignalsComponent } from './components/signals/signals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RxjsComponent,
+    SignalsComponent
   ],
   imports: [
     BrowserModule,
